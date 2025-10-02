83dfaad740c71b259b8db4c0b2f2079519b3712d
Birkan A.F. <59991587+ksk1kan@users.noreply.github.com>
2025-10-02 16:39:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["İYİ Kİ DOĞDUN","EMRE"],
+    strings: ["İYİ Kİ DOĞDUN","FAKATMÜZEYYEN :)"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
